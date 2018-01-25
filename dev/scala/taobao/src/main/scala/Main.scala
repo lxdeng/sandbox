923df8764e6b40aea278f2cf46cd0c0e17068173
lxdeng <buyibabe@gmail.com>
2018-01-25 12:15:47 -0800

    Scala immport

--- dev/scala/taobao/src/main/scala/Main.scala
+++ dev/scala/taobao/src/main/scala/Main.scala
@@ -1,3 +1,15 @@
+import com.taobao.Company
+import com.taobao.hr.Employee
+
 object Main extends App {
   println("Hello, World!")
+
+  val c = new com.taobao.Company("Yun Ma")
+  c.ceoGreet()
+
+  val c2 = new Company("Yun Ma")
+  c2.ceoGreet()
+
+  val emp = new Employee("Xiaoer", 18)
+  emp.works()
 }
---
+++ dev/scala/taobao/src/main/scala/com/taobao/hr/Employee.scala
@@ -0,0 +1,9 @@
+package com.taobao.hr
+
+class Employee(name: String, age: Int) {
+
+  def works() {
+     printf("Empployee %s works\n", name)
+  }
+}
+
